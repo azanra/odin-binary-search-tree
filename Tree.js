f52cee265601e0e4eae0d30668dc04cd73df9257
azan-personal <112457939+azanra@users.noreply.github.com>
2026-01-13 12:02:15 +0700

    feat: add method to visualize the binary tree

--- Tree.js
+++ Tree.js
@@ -27,24 +27,47 @@ class Tree {
 
   buildTree(array) {
     const uniqueAndSortedArray = [...new Set(array)].sort((a, b) => a - b);
+    console.log(`${uniqueAndSortedArray} \n`);
 
-    return this.TraverseToBuild(
+    return this.traverseToBuild(
       uniqueAndSortedArray,
       0,
       uniqueAndSortedArray.length - 1
     );
   }
 
-  TraverseToBuild(array, start, end) {
+  traverseToBuild(array, start, end) {
     if (start > end) return null;
 
     const middle = Math.floor((start + end) / 2);
     const root = new Node(array[middle]);
 
-    root.leftChildren = this.TraverseToBuild(array, start, middle - 1);
-    root.rightChildren = this.TraverseToBuild(array, middle + 1, end);
+    root.leftChildren = this.traverseToBuild(array, start, middle - 1);
+    root.rightChildren = this.traverseToBuild(array, middle + 1, end);
 
     return root;
+  }
+
+  prettyPrint(node, prefix = "", isLeft = true) {
+    if (node === null) return;
+
+    if (node.rightChildren !== null) {
+      this.prettyPrint(
+        node.rightChildren,
+        `${prefix}${isLeft ? "|   " : "    "}`,
+        false
+      );
+    }
+
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+
+    if (node.leftChildren !== null) {
+      this.prettyPrint(
+        node.leftChildren,
+        `${prefix}${isLeft ? "    " : "|    "}`,
+        true
+      );
+    }
   }
 }
 
--- main.js
+++ main.js
@@ -3,3 +3,4 @@ import Tree from "./Tree.js";
 const array = [4, 2, 1, 4, 3, 5, 7, 23, 656, 31];
 
 const binaryTree = new Tree(array);
+binaryTree.prettyPrint(binaryTree.root);
